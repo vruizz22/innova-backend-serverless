b93ceb51ed52e9ac3aec79ff3c47ed5185054c12
Victor Ruiz <veruiz@uc.cl>
2026-04-20 04:58:35 -0400

    feat(tests): update test to reflect correct greeting message

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Innova Backend Serverless is running!"', () => {
+      expect(appController.getHello()).toBe('Innova Backend Serverless is running!');
     });
   });
 });
