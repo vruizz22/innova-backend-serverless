3a8094a41bb584b7abe28748ba21c229031e114f
Victor Ruiz <veruiz@uc.cl>
2026-04-20 06:35:27 -0400

    fix(app): update import paths to use alias for better consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './infrastructure/database/database.module';
-import { TelemetryModule } from './infrastructure/telemetry.module';
-import { ProfilesModule } from './profiles/profiles.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from '@/infrastructure/database/database.module';
+import { TelemetryModule } from '@/infrastructure/telemetry.module';
+import { ProfilesModule } from '@/profiles/profiles.module';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
 
 @Module({
   imports: [DatabaseModule, TelemetryModule, ProfilesModule],
