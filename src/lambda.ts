10ceb9ece9d336dfe44a145c27d73c7de0efcf9f
Victor Ruiz <veruiz@uc.cl>
2026-04-20 06:35:10 -0400

    refactor(lambda): improve type annotations and update import path for AppModule

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,17 +1,30 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import { Handler, Context, Callback } from 'aws-lambda';
 
-let cachedServer;
+let cachedServer: Handler;
 
-export const handler = async (event, context) => {
+export const handler: Handler = async (
+  event: unknown,
+  context: Context,
+  callback: Callback,
+) => {
   if (!cachedServer) {
     const nestApp = await NestFactory.create(AppModule);
     nestApp.enableCors();
     await nestApp.init();
+
+    const expressApp = nestApp.getHttpAdapter().getInstance() as Parameters<
+      typeof serverlessExpress
+    >[0]['app'];
+
     cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
+      app: expressApp,
     });
   }
-  return cachedServer(event, context);
-};
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const result = await cachedServer(event as any, context, callback);
+  return result as unknown;
+  // eslint-disable-next-line prettier/prettier
+};;
