14bbec4098a1d9cbd9ba31645cedae908d855077
Victor Ruiz <veruiz@uc.cl>
2026-04-20 06:31:27 -0400

    fix(tests): update import path for AppModule to use alias

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '@/app.module';
 
 jest.mock('mongoose', () => {
   return {
@@ -45,6 +45,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
