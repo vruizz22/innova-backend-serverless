fce780b4a57993ddf538f782db9d0195e72bd2ba
Victor Ruiz <veruiz@uc.cl>
2026-04-20 04:25:18 -0400

    feat(telemetry): initialize TelemetryModule with Mongoose integration and controllers

--- src/telemetry/telemetry.module.ts
+++ src/telemetry/telemetry.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TelemetryController } from './telemetry.controller';
+import { TelemetryService } from './telemetry.service';
+import {
+  RawTelemetry,
+  RawTelemetrySchema,
+} from './schemas/raw-telemetry.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: RawTelemetry.name, schema: RawTelemetrySchema },
+    ]),
+  ],
+  controllers: [TelemetryController],
+  providers: [TelemetryService],
+})
 export class TelemetryModule {}
