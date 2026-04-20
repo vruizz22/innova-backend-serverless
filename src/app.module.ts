f0c07ad717f68e3bd30f1f78beb75b9bb3963253
Victor Ruiz <veruiz@uc.cl>
2026-04-20 04:03:09 -0400

    feat(app): implement AppController and AppService with basic hello world functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { TelemetryModule } from './telemetry/telemetry.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, TelemetryModule, ProfilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
