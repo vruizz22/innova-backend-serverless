8392120981088cb82758e27f3538b2852d183415
Victor Ruiz <veruiz@uc.cl>
2026-04-30 18:25:05 -0400

    feat(guard): add JwtAuthGuard for JWT authentication

---
+++ src/modules/auth/jwt-auth.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    void context;
+    return true;
+  }
+}
