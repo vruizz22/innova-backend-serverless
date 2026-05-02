cf5b591bb1e093548a986679f122a90c6cec9a29
Victor Ruiz <veruiz@uc.cl>
2026-05-02 16:02:28 -0400

    feat(auth/module): add AuthController, AuthService, and AuthTokenService to AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,14 +6,20 @@ import { JwtStrategy } from '@modules/auth/jwt.strategy';
 import { RolesGuard } from '@modules/auth/roles.guard';
 import { DatabaseModule } from '@infrastructure/database/database.module';
 import { UsersService } from '@modules/auth/users.service';
+import { AuthController } from '@modules/auth/auth.controller';
+import { AuthService } from '@modules/auth/auth.service';
+import { AuthTokenService } from '@modules/auth/auth-token.service';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     DatabaseModule,
   ],
+  controllers: [AuthController],
   providers: [
     UsersService,
+    AuthService,
+    AuthTokenService,
     JwtStrategy,
     JwtAuthGuard,
     {
