26ff5553c922a5f52bf60938bf44e0a27de01e44
Victor Ruiz <veruiz@uc.cl>
2026-05-02 06:58:08 -0400

    feat(auth): add DatabaseModule and UsersService to AuthModule imports and providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,16 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtAuthGuard } from '@modules/auth/jwt-auth.guard';
 import { JwtStrategy } from '@modules/auth/jwt.strategy';
 import { RolesGuard } from '@modules/auth/roles.guard';
+import { DatabaseModule } from '@infrastructure/database/database.module';
+import { UsersService } from '@modules/auth/users.service';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    DatabaseModule,
+  ],
   providers: [
+    UsersService,
     JwtStrategy,
     JwtAuthGuard,
     {
