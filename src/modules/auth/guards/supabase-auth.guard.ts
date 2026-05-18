97dcef0d87e85561bfce9ac685af45d072800696
Victor Ruiz <veruiz@uc.cl>
2026-05-18 00:59:25 -0400

    feat(auth): implement SupabaseAuthGuard for JWT authentication

---
+++ src/modules/auth/guards/supabase-auth.guard.ts
@@ -0,0 +1,24 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '@modules/auth/public.decorator';
+
+@Injectable()
+export class SupabaseAuthGuard extends AuthGuard('supabase-jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  override canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
